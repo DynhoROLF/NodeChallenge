5c61f8a4cb95b1152f707d8e82e7f5d4410f60c0
DynhoROLF <dynhoromero@gmail.com>
2025-09-10 19:56:12 -0300

    3rd class in node class

---
+++ src/routes/get-course-by-id.test.ts
@@ -0,0 +1,31 @@
+import { expect, test } from 'vitest'
+import request from 'supertest'
+import { server } from '../app.ts'
+import { makeCourse } from '../tests/factories/make-course.ts'
+
+test('get course by id', async () => {
+  await server.ready()
+
+  const course = await makeCourse()
+
+  const response = await request(server.server)
+    .get(`/courses/${course.id}`)
+
+  expect(response.status).toEqual(200)
+  expect(response.body).toEqual({
+    course: {
+      id: expect.any(String),
+      title: expect.any(String),
+      description: null,
+    }
+  })
+})
+
+test('return 404 for non existing courses', async () => {
+  await server.ready()
+
+  const response = await request(server.server)
+    .get(`/courses/5f5369ce-53d7-4078-9d8e-2d1b5c8cfc16`)
+
+  expect(response.status).toEqual(404)
+})
